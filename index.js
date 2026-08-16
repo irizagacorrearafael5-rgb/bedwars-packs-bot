fe9b4db524625bc8797741b5a04f6d3c61c07dc7
irizagacorrearafael5-rgb <irizagacorrearafael5@gmail.com>
2026-08-16 01:26:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const cron = require('node-cron');
+const { runAutoSearch } = require('./src/autoSearch');
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
@@ -9,6 +11,14 @@ client.commands = new Collection();
 
 client.once('ready', () => {
   console.log(`✅ Bot online como ${client.user.tag}`);
+
+  // Roda a busca automática assim que o bot liga
+  runAutoSearch(client);
+
+  // Depois roda a cada 30 minutos
+  cron.schedule('*/30 * * * *', () => {
+    runAutoSearch(client);
+  });
 });
 
 client.login(process.env.DISCORD_TOKEN);
